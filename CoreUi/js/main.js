e5e74004c5bd95a4585410552eca58d4dc3d501f
Nicat <stablizator@gmail.com>
2018-07-01 13:53:38 +0400

    Add files via upload

---
+++ CoreUi/js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    
+    });
+
+
+
+    function ghost() {
+        var x = document.getElementById("dropdown");
+        if (x.style.display == "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    
+    function w3_open() {
+        document.getElementById("wrapper").style.marginLeft = "25%";
+        document.getElementById("sidebar").style.width = "25%";
+        document.getElementById("sidebar").style.display = "block";
+        document.getElementById("openNav").style.display = 'none';
+      }
+      function w3_close() {
+        document.getElementById("wrapper").style.marginLeft = "0%";
+        document.getElementById("sidebar").style.display = "none";
+        document.getElementById("openNav").style.display = "inline-block";
+      }
