5c19977a073105842e31daed648c4bfdfa541634
Nicat <stablizator@gmail.com>
2018-07-07 13:29:31 +0400

    Add files via upload

--- CoreUi/js/main.js
+++ CoreUi/js/main.js
@@ -10,6 +10,14 @@ $(document).ready(function () {
 
     function ghost() {
         var x = document.getElementById("dropdown");
+        if (x.style.display == "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "none";
+        }
+    }
+    function ghostic() {
+        var x = document.getElementById("dropdownSecond");
         if (x.style.display == "none") {
             x.style.display = "block";
         } else {
@@ -17,14 +25,14 @@ $(document).ready(function () {
         }
     }
     
-    function w3_open() {
-        document.getElementById("wrapper").style.marginLeft = "25%";
-        document.getElementById("sidebar").style.width = "25%";
-        document.getElementById("sidebar").style.display = "block";
-        document.getElementById("openNav").style.display = 'none';
-      }
-      function w3_close() {
-        document.getElementById("wrapper").style.marginLeft = "0%";
-        document.getElementById("sidebar").style.display = "none";
-        document.getElementById("openNav").style.display = "inline-block";
-      }
+    // function w3_open() {
+    //     document.getElementById("wrapper").style.marginLeft = "25%";
+    //     document.getElementById("sidebar").style.width = "25%";
+    //     document.getElementById("sidebar").style.display = "block";
+    //     document.getElementById("openNav").style.display = 'none';
+    //   }
+    //   function w3_close() {
+    //     document.getElementById("wrapper").style.marginLeft = "0%";
+    //     document.getElementById("sidebar").style.display = "none";
+    //     document.getElementById("openNav").style.display = "inline-block";
+    //   }
